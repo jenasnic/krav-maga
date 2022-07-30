c80d1f025178638639666d3b72aabfd185c684e1
JC <jc@jc.com>
2022-07-30 21:53:10 +0200

    Update model

---
+++ src/Form/Type/AddressType.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\ValueObject\Address;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataMapperInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+use Symfony\Component\Form\Exception\UnexpectedTypeException;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Traversable;
+use TypeError;
+
+class AddressType extends AbstractType implements DataMapperInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('street', TextType::class)
+            ->add('zipCode', TextType::class)
+            ->add('city', TextType::class)
+            ->setDataMapper($this)
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Address::class,
+            'empty_data' => null,
+            'error_bubbling' => false,
+        ]);
+    }
+
+    public function mapDataToForms($viewData, Traversable $forms): void
+    {
+        if (null === $viewData) {
+            return;
+        }
+
+        if (!$viewData instanceof Address) {
+            throw new UnexpectedTypeException($viewData, Address::class);
+        }
+
+        $forms = iterator_to_array($forms);
+
+        // initialize form field values
+        $forms['street']->setData($viewData->getStreet());
+        $forms['zipCode']->setData($viewData->getZipCode());
+        $forms['city']->setData($viewData->getCity());
+    }
+
+    public function mapFormsToData(Traversable $forms, &$viewData): void
+    {
+        $forms = iterator_to_array($forms);
+
+        $allEmpty = empty(array_map(function (FormInterface $form) {
+            return $form->getData();
+        }, $forms));
+
+        try {
+            /** @var string|null $street */
+            $street = $forms['street']->getData();
+            /** @var string|null $zipCode */
+            $zipCode = $forms['zipCode']->getData();
+            /** @var string|null $city */
+            $city = $forms['city']->getData();
+
+            $viewData = new Address($street, $zipCode, $city);
+        } catch (TypeError $error) {
+            if ($allEmpty) {
+                $viewData = null;
+            } else {
+                throw new TransformationFailedException();
+            }
+        }
+    }
+}
---
+++ src/ValueObject/Address.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\ValueObject;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Embeddable]
+class Address
+{
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[Assert\NotBlank]
+    private ?string $street;
+
+    #[ORM\Column(type: 'string', length: 25, nullable: true)]
+    #[Assert\NotBlank]
+    private ?string $zipCode;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    #[Assert\NotBlank]
+    private ?string $city;
+
+    public function __construct(
+        ?string $street = null,
+        ?string $zipCode = null,
+        ?string $city = null,
+    ) {
+        $this->street = $street;
+        $this->zipCode = $zipCode;
+        $this->city = $city;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function getZipCode(): ?string
+    {
+        return $this->zipCode;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+}
