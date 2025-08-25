fa8a143456a565bb0bf7c4d0a21982b6b21c8553
JC <jc@jc.com>
2025-08-25 22:49:41 +0200

    Fix adherent form on front (no kmis)

--- src/Form/AdherentType.php
+++ src/Form/AdherentType.php
@@ -29,6 +29,9 @@ class AdherentType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        /** @var bool $forKmis */
+        $forKmis = $options['kmis_version'];
+
         $builder
             ->add('firstName', TextType::class, [
                 'disabled' => $options['re_enrollment'],
@@ -60,11 +63,14 @@ class AdherentType extends AbstractType
             ->add('address', AddressType::class, [
                 'label' => false,
             ])
-            ->add('reEnrollmentToNotify', CheckboxType::class, [
+        ;
+
+        if ($forKmis) {
+            $builder->add('reEnrollmentToNotify', CheckboxType::class, [
                 'required' => false,
                 'help' => 'form.adherent.reEnrollmentToNotifyHelp',
-            ])
-        ;
+            ]);
+        }
 
         $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($options) {
             $form = $event->getForm();
